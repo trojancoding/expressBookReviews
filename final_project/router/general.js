49f4bdaa87c93b3e86ce37b9a02cbfd1ae5d463c
trojancoding <76630415+trojancoding@users.noreply.github.com>
2025-02-15 00:56:20 +0100

    Simulate async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,63 +23,102 @@ public_users.post("/register", (req, res) => {
   }
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop using Promises
 public_users.get("/", function (req, res) {
-  const formattedBooks = JSON.stringify(books, null, 2);
-  res.status(200).type("json").send(formattedBooks);
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books);
+    }, 0);
+  })
+    .then((booksData) => {
+      const formattedBooks = JSON.stringify(booksData, null, 2);
+      res.status(200).type("json").send(formattedBooks);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error fetching books" });
+    });
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  const book = books[isbn];
-
-  if (book) {
-    const formattedBook = JSON.stringify(book, null, 2);
-    res.status(200).type("json").send(formattedBook);
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const book = books[isbn];
+      if (book) {
+        resolve(book);
+      } else {
+        reject("Book not found");
+      }
+    }, 0);
+  })
+    .then((book) => {
+      const formattedBook = JSON.stringify(book, null, 2);
+      res.status(200).type("json").send(formattedBook);
+    })
+    .catch((error) => {
+      res.status(404).json({ message: error });
+    });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   const authorParam = req.params.author;
-  const matchingBooks = {};
 
-  Object.keys(books).forEach((isbn) => {
-    const book = books[isbn];
-    if (book.author === authorParam) {
-      matchingBooks[isbn] = book;
-    }
-  });
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const matchingBooks = {};
+      Object.keys(books).forEach((isbn) => {
+        const book = books[isbn];
+        if (book.author === authorParam) {
+          matchingBooks[isbn] = book;
+        }
+      });
 
-  if (Object.keys(matchingBooks).length > 0) {
-    const formattedBooks = JSON.stringify(matchingBooks, null, 2);
-    res.status(200).type("json").send(formattedBooks);
-  } else {
-    res.status(404).json({ message: "No books found by this author" });
-  }
+      if (Object.keys(matchingBooks).length > 0) {
+        resolve(matchingBooks);
+      } else {
+        reject("No books found by this author");
+      }
+    }, 0);
+  })
+    .then((matchingBooks) => {
+      const formattedBooks = JSON.stringify(matchingBooks, null, 2);
+      res.status(200).type("json").send(formattedBooks);
+    })
+    .catch((error) => {
+      res.status(404).json({ message: error });
+    });
 });
 
-// Get all books based on title
+// Get book details based on title using Promises
 public_users.get("/title/:title", function (req, res) {
   const titleParam = req.params.title;
-  const matchingBooks = {};
 
-  Object.keys(books).forEach((isbn) => {
-    const book = books[isbn];
-    if (book.title === titleParam) {
-      matchingBooks[isbn] = book;
-    }
-  });
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const matchingBooks = {};
+      Object.keys(books).forEach((isbn) => {
+        const book = books[isbn];
+        if (book.title === titleParam) {
+          matchingBooks[isbn] = book;
+        }
+      });
 
-  if (Object.keys(matchingBooks).length > 0) {
-    const formattedBooks = JSON.stringify(matchingBooks, null, 2);
-    res.status(200).type("json").send(formattedBooks);
-  } else {
-    res.status(404).json({ message: "No books found by this title" });
-  }
+      if (Object.keys(matchingBooks).length > 0) {
+        resolve(matchingBooks);
+      } else {
+        reject("No books found by this title");
+      }
+    }, 0);
+  })
+    .then((matchingBooks) => {
+      const formattedBooks = JSON.stringify(matchingBooks, null, 2);
+      res.status(200).type("json").send(formattedBooks);
+    })
+    .catch((error) => {
+      res.status(404).json({ message: error });
+    });
 });
 
 //  Get book review
